43cbf586dbfa983576abcc2e12d5173cf6bec73d
Wira Dharma Kencana Putra <>
2020-03-01 20:51:49 +0700

    Add course material

---
+++ 16 - Recurrent Neural Network/utils.py
@@ -0,0 +1,45 @@
+import torch
+import matplotlib.pyplot as plt
+    
+def data4pred(model, dataset, dataloader, device):    
+    preds, targets = [], []
+    hidden = None
+    with torch.no_grad():
+        model.eval()
+        for inputs, target in dataloader:
+            inputs = inputs.to(device)
+            
+            output, hidden = model(inputs, hidden)
+            preds += output.flatten().tolist()
+            targets += target.flatten().tolist()
+
+    plt.plot(dataset.target_ticks, targets, 'b-', label="data")
+    plt.plot(dataset.target_ticks, preds, 'r-', label="pred")
+    plt.legend()    
+    
+def pred4pred(model, dataset, dataloader, device, n_prior=500, n_forecast=200):
+    preds, targets = [], []
+    hidden = None    
+    end = n_prior + n_forecast    
+    with torch.no_grad():
+        model.eval()       
+        for idx, (inputs, target) in enumerate(dataloader):
+            if idx == end:
+                break
+            elif idx > n_prior:
+                inputs[0, 0, 0] = preds[-1]
+                
+            inputs = inputs.to(device)
+            output, hidden = model(inputs, hidden)
+            
+            if idx > n_prior:
+                preds.append(output.flatten().tolist()[-1])
+            else:
+                preds += output.flatten().tolist()   
+            targets += target.flatten().tolist()
+
+    plt.plot(dataset.target_ticks[:n_prior], targets[:n_prior], 'b-', label="history_data")
+    plt.plot(dataset.target_ticks[n_prior:end], targets[n_prior:], 'b-', label="unseen_data", alpha=0.3)
+    plt.plot(dataset.target_ticks[:end], preds, 'r-', label="prediction")
+    plt.axvline(dataset.target_ticks[n_prior], color='k', linestyle="--", linewidth=1)
+    plt.legend()            
